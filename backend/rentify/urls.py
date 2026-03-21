8fcb92ae267de3cdefaa8c43bce5ed1c4737ea25
Amira Salah <salahamira358@gmail.com>
2026-03-21 22:04:30 +0100

    fix: add newline at end of rentify/urls.py

--- backend/rentify/urls.py
+++ backend/rentify/urls.py
@@ -39,5 +39,4 @@ urlpatterns = [
         name="swagger-ui",
     ),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("", RootView.as_view(), name="root"),
 ]
