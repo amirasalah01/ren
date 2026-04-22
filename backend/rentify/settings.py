1a006f4ad2a708d7acf5bdeb4d8968c9be3c2c75
Amira Salah <salahamira358@gmail.com>
2026-04-22 21:53:33 +0100

    add backend

--- backend/rentify/settings.py
+++ backend/rentify/settings.py
@@ -93,11 +93,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME", "your_db_name"),
-            "USER": os.getenv("DB_USER", "your_db_user"),
-            "PASSWORD": os.getenv("DB_PASSWORD", "your_db_password"),
-            "HOST": os.getenv("DB_HOST", "localhost"),
-            "PORT": os.getenv("DB_PORT", "5432"),
+            "NAME": config("DB_NAME", default="rentify"),
+            "USER": config("DB_USER", default="postgres"),
+            "PASSWORD": config("DB_PASSWORD", default=""),
+            "HOST": config("DB_HOST", default="localhost"),
+            "PORT": config("DB_PORT", default="5432"),
         }
     }
 
